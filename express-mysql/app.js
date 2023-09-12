7fdb975b2699562be059363c0a94e0b184c2182d
Tang <lengsukq@foxmail.com>
2023-09-12 11:14:43 +0800

    实现数据库的读取

--- express-mysql/app.js
+++ express-mysql/app.js
@@ -1,25 +1,42 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const logger = require('./logger');
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+const connection = require('./src/db'); // 获取连接实例
 
-app.use('/', indexRouter);
+// var app = express()
+
+app.get('/', (req, res, next) => {
+  /* 使用 connection.query 来执行 sql 语句 */
+  // 第一个参数为 sql 语句，可以透过 js 自由组合
+  // 第二个参数为回调函数，err 表示查询异常、第二个参数则为查询结果（这里的查询结果为多个用户行）
+  connection.query('select * from user', (err, users) => {
+    if (err) {
+      res.send('query error')
+    } else {
+      // 将 MySQL 查询结果作为路由返回值
+      res.send(users)
+    }
+  })
+})
+
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -27,15 +44,27 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// error handler app错误处理
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+// 处理非404错误（throw出来的错误）
+const _errorHandler = (err, req, res, next) => {
+  logger.error(`${req.method} ${req.originalUrl}` + err.message)
+  const errorMsg = err.message
+  res.status(err.status || 500).json({
+    code: -1,
+    success: false,
+    message: errorMsg,
+    data:{}
+  })
+}
+app.use(_errorHandler)
 
 module.exports = app;
