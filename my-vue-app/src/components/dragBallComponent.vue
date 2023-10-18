6948a5a44914663c98c16c9549d69bdf36c7fcb9
Tang <lengsukq@foxmail.com>
2023-10-18 18:04:58 +0800

    第一版悬浮球

--- my-vue-app/src/components/dragBallComponent.vue
+++ my-vue-app/src/components/dragBallComponent.vue
@@ -1,138 +1,224 @@
 <template>
-  <div class="drag-ball" ref="dragBall" @touchstart.stop.prevent="touchstart" @touchmove.stop.prevent="touchmove"
-       @touchend.stop.prevent="touchend">
-    <div class="drag-content">
-      <slot name="value">{{ value }}</slot>
+  <!--    全屏容器    -->
+  <div ref="pageDiv" @mousemove="demo_move" @mouseup="demo_up"
+       :class="{'zlevelTop':mouseDownState}"
+       style="position: absolute;top: 0;height: 100%;width: 100%">
+    <!--  点击蒙版  -->
+    <div v-if="menuOpen"  @click.stop="closeOpenModal"
+         style="position: fixed;top: 0;left: 0;width: 100%;height: 100%;z-index: 998"></div>
+    <!--  多功能菜单 -->
+    <div :class="{'six-more-modal-btn':menuOpen,'moreModal':!menuOpen,'more-tran-animate':!mouseDownState}"
+         ref="actionMgr" :style="position"  @mousedown="demo_down">
+      <!--  触发器 -->
+      <div v-if="!menuOpen" @click="demo_click" class="imgMore">
+        <img class="more-img" :src="" alt="" title="多功能菜单"/>
+      </div>
+      <!--  菜单  -->
+      <div v-else></div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-import {ref} from "vue";
-const props = defineProps({
-  value: {
-    type: String,
-    default: '悬浮球！'
-  }
-})
-
-const canDrag = ref(false);
-const dragBall = ref(null);
-// 初始位置
-const positionOld = ref({
-  left: 0,
-  top: 0
-});
-// 位置
-const position = ref({
-  left: 0,
-  top: 0
-})
-const inset = ref({
-  left: 0,
-  top: 0
-})
-// 移动
-const move = ref({})
-const touchstart = (e) => {
-  if (!canDrag.value) {
-    startTime.value = e.timeStamp;
-    positionOld.value = getPosition(dragBall.value);
-    position.value = positionOld.value;
-    inset.value = {
-      left: e.targetTouches[0].clientX - positionOld.value.left,
-      top: e.targetTouches[0].clientY - positionOld.value.top
-    };
-    canDrag.value = true;
-  }
-}
-
-const touchmove = (e) => {
-  if (canDrag.value) {
-    let left = e.targetTouches[0].clientX - inset.value.left;
-    let top = e.targetTouches[0].clientY - inset.value.top;
-    if (left < 0) {
-      left = 0;
-    } else if (left > (window.innerWidth - dragBall.value.offsetWidth)) {
-      left = window.innerWidth - dragBall.value.offsetWidth;
-    }
-    if (top < 0) {
-      top = 0;
-    } else if (top > (window.innerHeight - dragBall.value.offsetHeight)) {
-      top = window.innerHeight - dragBall.value.offsetHeight;
-    }
-    dragBall.value.style.left = left + 'px';
-    dragBall.value.style.top = top + 'px';
-    move.value = {
-      x: left - positionOld.value.left,
-      y: top - positionOld.value.top
-    };
-    position.value = {left, top};
-  }
-}
-const endTime = ref('');
-const startTime = ref('');
-const emit = defineEmits(['click'])
-const touchend = (e) => {
-  if (canDrag.value) {
-    endTime.value = e.timeStamp;
-    if (endTime.value - startTime.value > 400 || Math.abs(move.value.x) > 2 || Math.abs(move.value.y) > 2) {
-      // 非单击事件
-      if ((position.value.left + dragBall.value.offsetWidth / 2) > window.innerWidth / 2) {
-        dragBall.value.style.left = window.innerWidth - dragBall.value.offsetWidth + 'px';
-      } else {
-        dragBall.value.style.left = 0 + 'px';
+<script>
+export default {
+  name: "homeDragbtn",
+  props: {
+    // 通过position来设置初始定位
+    position: {
+      type: Object,
+      default: function() {
+        return {
+          top: "32.25rem",
+          left: "0"
+        }
       }
-    } else {
-      emit('click');
     }
-    inset.value = {left: 0, top: 0};
-    move.value = {};
-    position.value = {left: 0, top: 0};
-    canDrag.value = false;
+  },
+  data() {
+    return {
+      menuOpen:false,     //  菜单展开状态
+      mouseDownState:false,   //  鼠标点击状态
+      iX:0, iY:0,
+      dX:0, dY:500,   //  初始定位
+      lastMoveIndex:0,    //  拖拽计数
+      curMoveIndex:0, //  历史计数
+    }
+  },
+  methods: {
+    //  鼠标按下
+    demo_down(event){
+      //  如果打开了菜单，则不做响应
+      if(this.menuOpen){
+        this.mouseDownState = false;
+        return
+      }
+      console.log("demo_down",event);
+      /* 此处判断  pc 或 移动端 得到 event 事件 */
+      var touch;
+      if (event.touches) {
+        touch = event.touches[0];
+      } else {
+        touch = event;
+      }
+      // 鼠标点击 面向页面 的 x坐标 y坐标
+      let { clientX, clientY } = touch;
+      // 鼠标x坐标 - 拖拽按钮x坐标  得到鼠标 距离 拖拽按钮 的间距
+      this.iX = clientX - this.$refs.actionMgr.offsetLeft;
+      // 鼠标y坐标 - 拖拽按钮y坐标  得到鼠标 距离 拖拽按钮 的间距
+      this.iY = clientY - this.$refs.actionMgr.offsetTop;
+      // 设置当前 状态为 鼠标按下
+      this.mouseDownState = true;
+    },
+    //  鼠标拖拽
+    demo_move(event){
+      //鼠标按下 切移动中
+      if (this.mouseDownState) {
+        console.log("demo_move",event);
+        /* 此处判断  pc 或 移动端 得到 event 事件 */
+        var touch;
+        if (event.touches) {
+          touch = event.touches[0];
+        } else {
+          touch = event;
+        }
+        // 鼠标移动时 面向页面 的 x坐标 y坐标
+        let { clientX, clientY } = touch;
+        //当前页面全局容器 dom 元素  获取容器 宽高
+        let {
+          clientHeight: pageDivY,
+          clientWidth: pageDivX
+        } = this.$refs.pageDiv;
+        /* 鼠标坐标 - 鼠标与拖拽按钮的 间距坐标  得到 拖拽按钮的 左上角 x轴y轴坐标 */
+        let [x, y] = [clientX - this.iX, clientY - this.iY];
+
+        //拖拽按钮 dom 元素  获取 宽高 style 对象
+        let {
+          clientHeight: actionMgrY,
+          clientWidth: actionMgrX,
+          style: actionMgrStyle
+        } = this.$refs.actionMgr;
+        /* 此处判断 拖拽按钮 如果超出 屏幕宽高 或者 小于
+           设置 屏幕最大 x=全局容器x y=全局容器y 否则 设置 为 x=0 y=0
+        */
+        if (x > pageDivX - actionMgrX) x = pageDivX - actionMgrX;
+        else if (x < 0) x = 0;
+        if (y > pageDivY - actionMgrY) y = pageDivY - actionMgrY;
+        else if (y < 0) y = 0;
+        this.dX =x;this.dY = y;
+        // 计算后坐标  设置 按钮位置
+        actionMgrStyle.left = `${x}px`;
+        actionMgrStyle.top = `${y}px`;
+        actionMgrStyle.bottom = "auto";
+        actionMgrStyle.right = "auto";
+        //  move Index
+        this.lastMoveIndex++;
+        //  当按下键滑动时， 阻止屏幕滑动事件
+        event.preventDefault();
+      }
+    },
+    //    鼠标抬起
+    demo_up(event){
+      console.log("demo_up",event);
+      //  当前页面全局容器 dom 元素  获取容器 宽高
+      let {
+        clientHeight: windowHeight,
+        clientWidth: windowWidth
+      } = document.documentElement;
+      console.log('全局容器:',windowWidth,windowHeight);
+      //  拖拽按钮 dom 元素  获取 宽高 style 对象
+      let {
+        clientHeight: actionMgrY,
+        clientWidth: actionMgrX,
+        style: actionMgrStyle
+      } = this.$refs.actionMgr;
+
+      console.log('拖拽按钮',actionMgrY,actionMgrX,actionMgrStyle);
+
+      // 计算后坐标  设置 按钮位置
+      if(this.dY>0&&this.dY<(windowHeight-50)){ //  不在顶部 且 不在底部
+        if(this.dX<=(windowWidth/2)){  //  left 小于等于屏幕一半
+          actionMgrStyle.left = 0;
+          actionMgrStyle.right = 'auto';
+        }else { //  left 大于屏幕一半
+          actionMgrStyle.left = 'auto';
+          actionMgrStyle.right = 0;
+        }
+        if(this.dY>=(windowHeight/2)){   //  宽度大于1/2时，是将top改为auto，调整bottom
+          actionMgrStyle.top = 'auto';
+          actionMgrStyle.bottom = (windowHeight - this.dY - 50) + 'px';
+        }
+      }else {
+        if(this.dY===0){  //  在顶部
+          actionMgrStyle.top = 0;
+          actionMgrStyle.bottom = 'auto';
+        }else if(this.dY===(windowHeight-50)){
+          actionMgrStyle.bottom = 0;
+          actionMgrStyle.top = 'auto';
+        }
+        if(this.dX>=(windowWidth/2)){  //  右侧是将left改为auto，调整right
+          actionMgrStyle.left = 'auto';
+          actionMgrStyle.right = (windowWidth - this.dX - 50) + 'px';
+        }
+      }
+      this.mouseDownState = false;
+    },
+    //    单击事件
+    demo_click(){
+      console.log("demo_click|moveIndex:",this.lastMoveIndex,this.curMoveIndex);
+      //  mouseup 后会激活click事件
+      //  如果上一次down事件到下一次click事件中经历10次以下move，则视为纯点击事件
+      if(this.lastMoveIndex-this.curMoveIndex<=10){
+        //  点击事件
+        this.menuOpen = !this.menuOpen;
+        if( this.menuOpen ){
+          //  打开菜单
+        }
+      }
+      this.curMoveIndex = this.lastMoveIndex
+    },
+    //    点击空白关闭菜单
+    closeOpenModal(){}
   }
 }
-
-// 获取dom的绝对位置
-const getPosition = (source) => {
-  let left = source.offsetLeft; //获取元素相对于其父元素的left值var left
-  let top = source.offsetTop;
-  let current = source.offsetParent; // 取得元素的offsetParent // 一直循环直到根元素
-  while (current != null) {
-    left += current.offsetLeft;
-    top += current.offsetTop;
-    current = current.offsetParent;
-  }
-  return {
-    left: left,
-    top: top
-  };
-}
-
 </script>
 <style scoped>
-.drag-ball {
-  position: absolute;
-  z-index: 10003;
-  right: 0;
-  top: 70%;
-  width: 20px;
-  height: 20px;
-  background: deepskyblue;
-  border-radius: 50%;
-  overflow: hidden;
-  box-shadow: 0px 0px 10px 2px skyblue;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 1em;
-  user-select: none;
+.zlevelTop{
+  z-index: 9999;
 }
-
-.drag-ball .drag-content {
-  overflow-wrap: break-word;
-  font-size: 14px;
+.more-tran-animate{
+  transition:0.5s;
+}
+.moreModal {
+  /* 如果碰到滑动问题，1.3 请检查 z-index。z-index需比web大一级*/
+  z-index: 9999;
+  position: fixed;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #337AB7;
+  line-height: 40px;
+  text-align: center;
   color: #fff;
-  letter-spacing: 2px;
+  opacity: 0.6;
+}
+.moreModal:hover{
+  opacity: 1;
+}
+.six-more-modal-btn{
+  position: fixed;
+  z-index: 9999;
+  width: 14rem;
+  height: 14rem;
+  border-radius: 5px;
+  background: #1A1A1A;
+  color: #fff;
+}
+.imgMore{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
 }
 </style>
